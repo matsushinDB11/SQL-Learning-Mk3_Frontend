b0cd2baf02653f1af430706b912c93ded238ca23
matsushinDB11 <matsushin35@gmail.com>
2021-12-05 01:01:48 +0900

    add env to api url

--- src/config.ts
+++ src/config.ts
@@ -1,9 +1,11 @@
 type _config = {
     GoogleClientID: string;
+    ApiUrl: string;
 };
 
 const Config: _config = {
     GoogleClientID: process.env.REACT_APP_GOOGLE_CLIENT_ID || '',
+    ApiUrl: process.env.REACT_APP_API_URL || '',
 };
 
 export default Config;
