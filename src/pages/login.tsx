6b5aff7d6508121c615fdb4b0e583ba37e6eece7
matsushinDB11 <matsushin35@gmail.com>
2021-12-06 00:55:39 +0900

    create login redirect

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -5,26 +5,31 @@ import GoogleLogin, {
     GoogleLoginResponseOffline,
 } from 'react-google-login';
 import Config from '../config';
+import { useHistory } from 'react-router-dom';
 
-const Success: () => void = () => {
-    console.log('Login Success');
+const Login: VFC = () => {
+    // TODO Set the user information obtained from the server in Context.
+    const history = useHistory();
+    const Success: () => void = () => {
+        console.log('Login Success');
+        history.push('/answer');
+    };
+    const Failure = (
+        response: GoogleLoginResponse | GoogleLoginResponseOffline,
+    ) => {
+        console.log(response);
+    };
+
+    return (
+        <Container sx={{ margin: 2 }}>
+            <GoogleLogin
+                clientId={Config.GoogleClientID}
+                buttonText="Login"
+                onSuccess={Success}
+                onFailure={Failure}
+            />
+        </Container>
+    );
 };
-
-const Failure = (
-    response: GoogleLoginResponse | GoogleLoginResponseOffline,
-) => {
-    console.log(response);
-};
-
-const Login: VFC = () => (
-    <Container sx={{ margin: 2 }}>
-        <GoogleLogin
-            clientId={Config.GoogleClientID}
-            buttonText="Login"
-            onSuccess={Success}
-            onFailure={Failure}
-        />
-    </Container>
-);
 
 export default Login;
